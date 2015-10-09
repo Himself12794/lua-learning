a4342fd4fe0fcf10f7112d8301356dfbb34e8023
Himself12794 <pwhiting.bba@gmail.com>
2015-10-09 03:48:45 -0400

    initial commit

---
+++ scripts/main.lua
@@ -0,0 +1,16 @@
+require 'math'
+-- It's been awhile since I used Lua
+local function main()
+
+  local i = 0
+  while true do
+    if i < 5 then
+      print(i)
+      i = i + 1
+    else
+      break
+    end
+  end
+
+end
+main()
